d2ea1e95b4ccb71139d1cbeaaca61596a4916d52
Ravindra <ravivedu@portl.co>
2022-11-29 20:36:34 +0530

    Point 2 I have added Location Permission in AndroidManifest.xml added AppLifecycleListener.kt, LocationPerDistance.kt files added lifecycle, google gms play-services-location component in app gradle file added lifecycle observer in Application app file added code to change of 10 meters of the current and previous location will reset the video and replay from the start

--- app/src/main/java/com/raveendra/testexoplayer/ui/MainActivity.kt
+++ app/src/main/java/com/raveendra/testexoplayer/ui/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.raveendra.testexoplayer.ui
 
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
@@ -15,12 +25,15 @@ import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import com.google.android.gms.maps.model.LatLng
 import com.raveendra.testexoplayer.R
 import com.raveendra.testexoplayer.utility.Constants
+import com.raveendra.testexoplayer.utility.Constants.DISTANCE_IN_METER
+import com.raveendra.testexoplayer.utility.LocationPerDistance
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationPerDistance.ILocationCallback {
 
     private val playerView: PlayerView by lazy {
         findViewById<PlayerView>(R.id.playerView)
@@ -30,14 +43,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var playButton: ImageButton
     private lateinit var player: SimpleExoPlayer
     private var isLauncher = false
+    private var locationPerDistance: LocationPerDistance? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pauseButton = playerView.findViewById(R.id.exo_pause)
         playButton = playerView.findViewById(R.id.exo_play)
         aspectRatioContainer.setAspectRatio(16f / 9)
+        requestLocationPermissions()
+        initialize()
+    }
+
+    private fun initialize() {
         initializePlayer()
         delayLaunch()
+        initializeLocation()
     }
 
     private fun initializePlayer() {
@@ -90,6 +110,29 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    //Location
+
+    private fun initializeLocation() {
+        locationBroadReceiverManager()
+        locationPerDistance = LocationPerDistance(
+            this,
+            this,
+            DISTANCE_IN_METER
+        )
+        locationPerDistance?.requestLocationUpdates()
+    }
+
+    private val locationBroadReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            intent.getStringExtra("STATUS")?.let { videoResetReplay() }
+        }
+    }
+
+    private fun locationBroadReceiverManager() {
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(locationBroadReceiver, IntentFilter("LOCATIONBROADRECEIVER"))
+    }
+
     override fun onResume() {
         super.onResume()
     }
@@ -115,7 +158,67 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         player.playWhenReady = false
         super.onDestroy()
+        locationPerDistance?.removeLocationUpdates()
     }
 
+    override fun onLocationChanged(location: LatLng?) {
+        videoResetReplay()
+    }
+
+    private fun videoResetReplay() {
+        player.seekTo(0)
+        player.setPlayWhenReady(true)
+    }
+
+    private fun requestLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1
+                )
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        when (requestCode) {
+            1 -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) === PackageManager.PERMISSION_GRANTED
+                    ) {
+                        locationPerDistance?.requestLocationUpdates()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/raveendra/testexoplayer/utility/Constants.kt
+++ app/src/main/java/com/raveendra/testexoplayer/utility/Constants.kt
@@ -3,4 +3,5 @@ package com.raveendra.testexoplayer.utility
 object Constants{
     const val VIDEO_URL : String = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
     const val DELAY_TIME: Long = 4000
+    const val DISTANCE_IN_METER = 10
 }
